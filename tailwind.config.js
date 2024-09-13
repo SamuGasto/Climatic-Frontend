09eda09181447dd8df269efaddf97d50c843aaf6
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-09-13 15:39:54 -0300

    Footer color fix, App colors added
    Se agregó la paleta de colores definida en figma dentro de la configuración de Tailwind. Se ajustó el footer para usar dichos colores donde corresponde.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#172127",
+        dark_resaltado: "#1E2B33",
+        principal: "#5B9279",
+        light_resaltado: "#F0F2F5",
+        principal_claro: "#90CB9B",
+        light: "#FFFFFF",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
