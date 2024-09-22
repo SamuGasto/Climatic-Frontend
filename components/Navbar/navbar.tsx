92276aa127290ae6c6df9c93142979ce3af892ea
bayronbenja <bayronastudillo11@gmail.com>
2024-09-21 23:08:50 -0300

    actualizando proyecto

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,3 +1,42 @@
-export const Navbar = () => {
-  return <div>navbar</div>;
-};
+"use client";
+import React from "react";
+import {Image,Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+export default function navbar() {
+  return (
+  <Navbar className="bg-white shadow-md">
+   <Navbar position="static">
+    <NavbarBrand>
+        <Image src="/logo.png" width={60} height={55}/>        
+        <p className="font-bold text-inherit">DiDema</p>    
+        </NavbarBrand>       
+        <NavbarContent className="hidden sm:flex gap-4" justify="end">
+          <NavbarItem isActive>
+            <Link color="foreground" href="#">
+            CLIMATIC
+            </Link>
+          </NavbarItem>
+        <NavbarItem> 
+          <Link color="foreground" href="#">
+           Re-Analisís
+           </Link>
+           </NavbarItem>
+           <NavbarItem>
+            <Link color="foreground" href="#" aria-current="page">
+            Preguntas Frecuentes
+          </Link>        
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">         
+          Sobre Nosotros
+          </Link>
+          </NavbarItem>
+          </NavbarContent>
+          <NavbarItem className="hidden lg:flex">       
+          </NavbarItem>
+        <NavbarItem>
+      </NavbarItem>
+      </Navbar> 
+    </Navbar>
+  );
+}      
