b93615a1da8d0c173980d3217b17632d1aec5567
SamuGasto <samuel.55321@gmail.com>
2024-09-26 23:52:53 -0300

    Disposición de tarjetas en el tablero
    Se añadió una tarjeta que puede agregar nuevos gráficos al tablero actual, falta implementar la funcionalidad.

--- components/Dashboard/Panel/board/has-board.tsx
+++ components/Dashboard/Panel/board/has-board.tsx
@@ -5,6 +5,7 @@ import ChartImage from "../chart-image";
 import Search from "./layout/search";
 import ButtonAddChart from "../add-chart";
 import Title from "./layout/title";
+import ChartsCards from "./Charts-Cards/charts-card";
 
 interface PropType {
   boardName: string;
@@ -16,39 +17,15 @@ function BoardPanel(props: PropType) {
   const { boardName, charts, seleccionarGrafico } = props;
 
   return (
-    <div>
-      <section className="flex flex-row w-full">
+    <div className="">
+      <section className="flex flex-row w-full mb-8">
         <div className="flex flex-col w-full gap-2 justify-items-start">
           <Title text={boardName} />
           <Search />
         </div>
         <ButtonAddChart />
       </section>
-      <div className="grid grid-flow-col gap-6 w-full h-full">
-        <div className="grid grid-flow-row grid-cols-4 gap-6 w-full h-full">
-          {charts.map((chart, index) => (
-            <Card
-              className="h-72 w-full"
-              key={index}
-              shadow="sm"
-              isPressable
-              onPress={() => seleccionarGrafico(chart)}
-            >
-              <CardHeader>
-                <div>
-                  <h1 className="text-xl font-semibold text-left">
-                    {chart.title}
-                  </h1>
-                  <h1>{chart.subtitle}</h1>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <ChartImage config={chart.inactiveConfig} />
-              </CardBody>
-            </Card>
-          ))}
-        </div>
-      </div>
+      <ChartsCards charts={charts} seleccionarGrafico={seleccionarGrafico} />
     </div>
   );
 }
