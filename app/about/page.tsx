7da0a36cd5a3a34fb445b676c1931ebc86506c54
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-10-03 20:11:24 -0300

    Create CardMember
    Creé el componente Cardmember para usar en sección sobre nosotros.

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import CardMember from "@/components/CardMember/card-member";
+
+export default function About() {
+    return (
+      <div className="flex flex-row justify-center self-center -mt-10">
+        <CardMember>
+
+        </CardMember>
+      </div>
+    );
+  }
---
+++ app/faq/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function page() {
+    return (
+      <div className="flex flex-row h-full w-full justify-center self-center -mt-10">
+        FAQ
+      </div>
+    );
+  }
