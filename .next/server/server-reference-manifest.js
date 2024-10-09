487220cff15d1a74488d3e30660a1de9a87bf57a
bayronbenja <bayronastudillo11@gmail.com>
2024-10-08 22:37:17 -0300

    Commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"74AzGwcIToxQTUo7KVH1CzrIQCMdcSYRfYi+3UZFjoo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C8zqQReJchK6GhFaJKTE5Fmn2vWJnjWYDcLSrShkJ+g=\"\n}"
