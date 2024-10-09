be69d65183c5db1538b58782423014a435943e72
bayronbenja <bayronastudillo11@gmail.com>
2024-10-08 23:01:32 -0300

    Actualizacion

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C8zqQReJchK6GhFaJKTE5Fmn2vWJnjWYDcLSrShkJ+g=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gUO6kOwi2MBQyzxgiwoLOekEAqjSrxIPtXtiY449JDc=\"\n}"
