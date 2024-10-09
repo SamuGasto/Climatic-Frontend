22e29c9cedd694d7e1424d80043083d1a51907ec
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-10-08 22:16:01 -0300

    modificaciones .next

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vDzor7XsAbWBbz5B/CQLoVDrVWlBasI4SkfbruXGT8Q=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8IreKgsolzQhvRzsaq1zkUHRZG/E3VFBuTz2l9Lajpo=\"\n}"
