dd8dfef82d6e83c67c8b391296dbb1fae76acc17
bayronbenja <bayronastudillo11@gmail.com>
2024-11-15 15:10:25 -0300

    cambios en Sidebar

--- components/Sidebar/opciones-variable.tsx
+++ components/Sidebar/opciones-variable.tsx
@@ -16,7 +16,6 @@ import { varContenidoVolumetrico } from "@/config/subvariables/var_contenido_vol
 import { varUnidadesTemperatura } from "@/config/subvariables/var_unidades_temperatura";
 import { unidadesTemperatura } from "@/config/subvariables/opcion_unidades_temperaturas";
 import { opcionesSerieTiempo } from "@/config/subvariables/opciones_serie_tiempo";
-import { typeChart } from "@/types/chart";
 
 type elemento = {
   key: string;
@@ -24,12 +23,12 @@ type elemento = {
 };
 
 type Props = {
-  setHayTiempo: (newTiempo: boolean) => void;
-  setVariable: (newVar: string) => void;
-  setNivel: (newNivel: number | null) => void;
-  setTypeChart: (newType: typeChart) => void;
-  setUnidadMedida: (newUnidad: string) => void;
-  setCalculoDatos: (newCalculoDatos: string) => void;
+  setHayTiempo: React.Dispatch<React.SetStateAction<boolean>>;
+  setVariable: React.Dispatch<React.SetStateAction<string>>;
+  setNivel: React.Dispatch<React.SetStateAction<number | null>>;
+  setTypeChart: React.Dispatch<React.SetStateAction<string>>;
+  setUnidadMedida: React.Dispatch<React.SetStateAction<string>>;
+  setCalculoDatos: React.Dispatch<React.SetStateAction<string>>;
   typeChart: string;
 };
 
@@ -169,18 +168,6 @@ export default function OpcionesVariable(props: Props) {
   );
 
   const cambiarTipoGrafico = (valor: string) => {
-    if (
-      valor !== "contorno" ||
-      "vectoriales" ||
-      "clasificacion" ||
-      "isobaras" ||
-      "lineas" ||
-      "dispersion" ||
-      "rosa_de_vientos" ||
-      "polares" ||
-      "barras"
-    )
-      return;
     setTypeChart(valor);
 
     let listAux1: elemento[] = [];
@@ -247,7 +234,7 @@ export default function OpcionesVariable(props: Props) {
           step={1}
           defaultValue={1}
           onChangeEnd={handleContenidoVolumetrico}
-          deshabilitado={false}
+          deshabilitado={true}
         />
       ) : null}
 
