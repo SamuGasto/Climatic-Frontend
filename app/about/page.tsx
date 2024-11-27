63862fe566b6316d63c2b9b3b5110d80d1fd69d0
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-11-27 12:11:30 -0300

    Update de About
    Se modifica página About, agregando logo y texto.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,26 @@
+"use client";
 import React from "react";
 import CardMember from "@/components/CardMember/card-member";
+import { Image } from "@nextui-org/react";
 
 export default function About() {
   return (
     <div className="justify-items-center -mt-10">
-      <h1 className="text-center text-4xl font-bold mb-2 mt-10">
+      <h1 className="text-center text-4xl font-bold mb-10 mt-10">
         Sobre Nosotros
       </h1>
-      <p className="text-center text-2xl mb-6 italic">Di-Dema</p>
-
-      <hr className="my-8 h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
+      {/* Imagen de la empresa */}
+      <div className="flex justify-center mb-3">
+        <Image
+          src="Di-Dema.png" // Ruta de la imagen del logo
+          alt="Logo de Di-Dema"
+          width={150}
+          height={150}
+          className="rounded-lg shadow-md"
+        />
+      </div>
+      <p className="text-center text-3xl font-bold">Di-Dema</p>
+      <p className= "text-center text-xl mb-4 italic">Web Development</p>
 
       <section className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-y-6 gap-x-5 md:gap-y-16 justify-items-center p-11 mx-0 lg:mx-72 mt-4 mb-14">
         <CardMember
@@ -19,7 +30,7 @@ export default function About() {
           bottom_text="Universidad de O'Higgins."
           color="amarillo"
           link_ref="http://www.linkedin.com/in/mauricio-gregorio-de-las-heras"
-          src="/Di-Dema-members/Mauricio_picture.png"
+          src="/Di-Dema-members/Mauricio_picture.jpeg"
         />
         <CardMember
           nombre="Samuel Ramírez"
@@ -28,7 +39,7 @@ export default function About() {
           bottom_text="Universidad de O'Higgins."
           color="verde"
           link_ref="http://www.linkedin.com/in/samuelramirezzuniga"
-          src="/Di-Dema-members/Samuel_picture.png"
+          src="/Di-Dema-members/Samuel_picture.jpeg"
         />
         <CardMember
           nombre="Nicolás Muñoz"
@@ -46,7 +57,7 @@ export default function About() {
           bottom_text="Universidad de O'Higgins."
           color="morado"
           link_ref="https://www.linkedin.com/in/bayron-astudillo-7a446128a/"
-          src="/Di-Dema-members/Bayron_picture.png"
+          src="/Di-Dema-members/Bayron_picture.jpeg"
         />
       </section>
     </div>
