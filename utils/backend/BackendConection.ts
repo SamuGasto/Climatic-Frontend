c118298f67c69cc379e4db3bcdc877cf41b408fc
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-12-14 12:24:24 -0300

    Cambios preparando producción
    Se hace el cambio en solicitudes del backend para producción.

--- utils/backend/BackendConection.ts
+++ utils/backend/BackendConection.ts
@@ -2,6 +2,8 @@ import { Consulta } from "@/types/consulta";
 import axios from "axios";
 
 export const RequestData = async (consulta: Consulta) => {
+  const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000";
+
   let time: string[] = [];
   if (consulta.tiempo) {
     time = [
@@ -15,11 +17,11 @@ export const RequestData = async (consulta: Consulta) => {
   try {
     let finalText = "";
     if (consulta.tiempo && consulta.nivel) {
-      finalText = `http://127.0.0.1:8000/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}/${time[0]}${time[1]}/${consulta.nivel}`;
+      finalText = `${BASE_URL}/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}/${time[0]}${time[1]}/${consulta.nivel}`;
     } else if (consulta.tiempo) {
-      finalText = `http://127.0.0.1:8000/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}/${time[0]}${time[1]}`;
+      finalText = `${BASE_URL}/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}/${time[0]}${time[1]}`;
     } else {
-      finalText = `http://127.0.0.1:8000/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}`;
+      finalText = `${BASE_URL}/zarr/${consulta.variable}/${consulta.typeChart}/${consulta.unidadMedida}/${consulta.latitud[0]},${consulta.latitud[1]}/${consulta.longitud[0]},${consulta.longitud[1]}`;
     }
 
     const res = (await axios.get(finalText)).data;
